05dc0743780795c63660df1365b14ae92f1331a7
macuser <KulDmitriBy@gmail.com>
2018-03-02 16:51:58 +0300

    add AddNew, Update

--- public/js/controllers/productController.js
+++ public/js/controllers/productController.js
@@ -14,11 +14,29 @@ InternetMarketApp.controller("productController", function ($scope, $http, $loca
         $scope.loadProducts();
     };
 
+    $scope.getCategories = function () {
+        $http({method: 'GET', url: 'categories/all'}).then(function success(categories) {
+            let options = [];
+            for (let i = 0; i < categories.data.length; i++) {
+                options.push({label: categories.data[i].name, _id: categories.data[i]._id})
+            }
+            $scope.options = options;
+        });
+    };
+
     $scope.updateById = function (id) {
         app.productId = id;
         $location.path("/editProductPage");
     };
 
+    $scope.findByCategoryId = function (id) {
+        $http({method: 'GET', url: 'products/findCategory/'+id}).then(function success(products) {
+            $scope.products = products.data;
+        });
+    };
+
+
+    $scope.options = $scope.getCategories();
     $scope.products = $scope.loadProducts();
 
 });
